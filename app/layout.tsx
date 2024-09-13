836056344e101990db08bd6520416b5948f106c7
BashkirovN <bashkirov.office@gmail.com>
2024-09-13 14:00:42 -0700

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+
+/*
+This is called a root layout and is required. 
+Any UI you add to the root layout will be shared across all pages in your application. 
+You can use the root layout to modify your <html> and <body> tags, and add metadata
+https://nextjs.org/learn/dashboard-app/creating-layouts-and-pages
+*/
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Bangers, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const bangers = Bangers({ subsets: ['latin'], weight: '400' });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
